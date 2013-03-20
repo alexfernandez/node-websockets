d31531e7cfc7ac4532cc0e55d1853e1ae1cff612
alexfernandez <alejandro@moveinblue.com>
2013-03-20 01:15:17 +0100

    Make the server listen for websocket requests.

--- src/server.js
+++ src/server.js
@@ -12,3 +12,31 @@ var server = http.createServer(serve).listen(port, function()
 {
 	console.log('Server running at http://127.0.0.1:' + port + '/');
 });
+
+/**
+ * Serve contents.
+ */
+function serve(request, response)
+{
+	// will serve websocket
+	return;
+}
+
+/**
+ * WebSocket server
+ */
+var wsServer = new webSocketServer({
+	// WebSocket server is tied to a HTTP server
+	httpServer: server
+});
+
+/**
+ * This callback function is called every time someone
+ * tries to connect to the WebSocket server
+ */
+wsServer.on('request', function(request)
+{
+	var connection = request.accept(null, request.origin);
+	console.log('Accepted connection for %s from %s', request.resource, connection.remoteAddress);
+});
+
